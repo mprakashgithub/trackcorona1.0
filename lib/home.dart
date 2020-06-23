ffbff8ce452fa784c9eb69e278ccebb4d13f1778
manish <manish@progmattic.com>
2020-06-23 07:56:57 +0530

    update 1.0

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:trackcorona/Response/IndiaResponse.dart';
-import 'package:trackcorona/global.dart';
 import 'package:trackcorona/india.dart';
-// import 'package:trackcorona/total.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -14,24 +11,24 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return DefaultTabController(
       initialIndex: 0,
-      length: 2,
+      length: 1,
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.blueAccent,
-          title: Text('Corona in India'),
+          title: Text('Corona in India', textAlign: TextAlign.center,),
           bottom: TabBar(
             tabs: <Widget>[
-              //Tab(text: 'Home',),
+          
               Tab(text: 'India',),
-              Tab(text: 'Corona Alert',),
+          
             ],
           ),
         ),
         body: TabBarView(
           children: <Widget>[
-           // Total(),
+          
             India(),
-            Global(),
+          
           ],
         ),
       )
